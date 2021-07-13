6f57215d58b727fd472afb4b19c11d8367d26464
Cristobal Medrano <cristobal.medrano@usach.cl>
2021-07-13 19:10:52 -0400

    EsLint y Autoidentación agregados

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import NavigationBar from './components/NavigationBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div>
+      <NavigationBar />
       Initial setup done.
     </div>
   );
